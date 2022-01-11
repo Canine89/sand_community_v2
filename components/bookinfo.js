da997d3c23ba40070bfe0121b1de8dbd34416d73
canine89 <phk707kr@gmail.com>
2022-01-11 16:55:42 +0900

    bookinfo style updated.

--- components/bookinfo.js
+++ components/bookinfo.js
@@ -3,25 +3,41 @@ export default function BookInfo({ isbnData }) {
 
   return (
     <div className="pl-4">
-      <span className="font-bold">
+      <span className="font-bold text-sm">
         {info.book.title} 책 정보({info.crawl_date.slice(0, 10)}일 기준)
       </span>
-      <div class="grid grid-cols-2 gap-4 pt-2 text-sm">
-        <div>순위</div>
-        <div>{info.rank}</div>
-        <div>판매지수</div>
-        <div>{info.sales_point}</div>
-        <div>ISBN</div>
-        <div>{info.book.isbn}</div>
-        <div>쪽수</div>
-        <div>{info.book.page}</div>
-        <div>출판사</div>
-        <div>{info.book.publisher}</div>
-        <div>정가</div>
-        <div>{info.book.right_price}원</div>
-        <div>10% 할인가</div>
-        <div>{info.book.sales_price}원</div>
-      </div>
+      <table className="text-sm table-auto">
+        <tbody>
+          <tr>
+            <td className="p-1 text-gray-500">순위</td>
+            <td className="p-1 text-gray-500">{info.rank}</td>
+          </tr>
+          <tr>
+            <td className="p-1 text-gray-500">판매지수</td>
+            <td className="p-1 text-gray-500">{info.sales_point}</td>
+          </tr>
+          <tr>
+            <td className="p-1 text-gray-500">ISBN</td>
+            <td className="p-1 text-gray-500">{info.book.isbn}</td>
+          </tr>
+          <tr>
+            <td className="p-1 text-gray-500">쪽수</td>
+            <td className="p-1 text-gray-500">{info.book.page}</td>
+          </tr>
+          <tr>
+            <td className="p-1 text-gray-500">출판사</td>
+            <td className="p-1 text-gray-500">{info.book.publisher}</td>
+          </tr>
+          <tr>
+            <td className="p-1 text-gray-500">정가</td>
+            <td className="p-1 text-gray-500">{info.book.right_price}</td>
+          </tr>
+          <tr>
+            <td className="p-1 text-gray-500">10% 할인가</td>
+            <td className="p-1 text-gray-500">{info.book.sales_price}</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
